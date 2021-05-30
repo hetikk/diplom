f33fa5319483eb7eb9da457135a3fe58f3c4c1d1
hetikk <h.kastuev99@inbox.ru>
2021-05-30 23:48:43 +0300

    fix

--- src/main/java/diplom/Tests.java
+++ src/main/java/diplom/Tests.java
@@ -8,11 +8,11 @@ import org.apache.commons.io.FileUtils;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Tests {
 
@@ -20,30 +20,42 @@ public class Tests {
 
     public static void main(String[] args) throws Exception {
 
-        final int[] wc = new int[]{5_000, 10_000, 15_000, 20_000, 25_000, 30_000};
-        final int[] fc = new int[]{5, 10, 15, 20};
+        Files.walk(Paths.get("C:\\Users\\hetikk\\IdeaProjects\\diplom\\src\\main\\java\\diplom"))
+                .filter(p -> p.getFileName().toString().endsWith("java"))
+                .forEach(f -> {
+                    System.out.println(f.getFileName());
+                    try {
+                        System.out.println(FileUtils.readFileToString(f.toFile(), Charset.defaultCharset()));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println();
+                });
 
-        List<String> list = readDictionary();
-
-        // При фикс числе доков меняю число слов
-        for (int wci : wc) {
-            String dir = "C:\\Users\\hetikk\\IdeaProjects\\diplom\\sets\\clustering\\6\\6.1\\" + wci + "\\";
-            for (int i = 0; i < 15; i++) {
-                Collections.shuffle(list);
-                String s = list.stream().limit(wci).collect(Collectors.joining(" "));
-                FileUtils.write(new File(dir + (i + 1) + ".txt"), s, Charset.defaultCharset());
-            }
-        }
-
-        // При фикс числе слов меняю колво доков
-        for (int fci : fc) {
-            String dir = "C:\\Users\\hetikk\\IdeaProjects\\diplom\\sets\\clustering\\6\\6.2\\" + fci + "\\";
-            for (int i = 0; i < fci; i++) {
-                Collections.shuffle(list);
-                String s = list.stream().limit(15_000).collect(Collectors.joining(" "));
-                FileUtils.write(new File(dir + fci + "." + (i + 1) + ".txt"), s, Charset.defaultCharset());
-            }
-        }
+//        final int[] wc = new int[]{5_000, 10_000, 15_000, 20_000, 25_000, 30_000};
+//        final int[] fc = new int[]{5, 10, 15, 20};
+//
+//        List<String> list = readDictionary();
+//
+//        // При фикс числе доков меняю число слов
+//        for (int wci : wc) {
+//            String dir = "C:\\Users\\hetikk\\IdeaProjects\\diplom\\sets\\clustering\\6\\6.1\\" + wci + "\\";
+//            for (int i = 0; i < 15; i++) {
+//                Collections.shuffle(list);
+//                String s = list.stream().limit(wci).collect(Collectors.joining(" "));
+//                FileUtils.write(new File(dir + (i + 1) + ".txt"), s, Charset.defaultCharset());
+//            }
+//        }
+//
+//        // При фикс числе слов меняю колво доков
+//        for (int fci : fc) {
+//            String dir = "C:\\Users\\hetikk\\IdeaProjects\\diplom\\sets\\clustering\\6\\6.2\\" + fci + "\\";
+//            for (int i = 0; i < fci; i++) {
+//                Collections.shuffle(list);
+//                String s = list.stream().limit(15_000).collect(Collectors.joining(" "));
+//                FileUtils.write(new File(dir + fci + "." + (i + 1) + ".txt"), s, Charset.defaultCharset());
+//            }
+//        }
     }
 
     static void createDictionary() throws IOException {
